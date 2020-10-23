a00ed8ea010d0c116aa9d4d314ecca9cea5a23f3
Doris Ding <ddddoris1@gmail.com>
2020-10-23 10:13:49 -0700

    snake game with no sound effect

--- src/main/java/SnakeGame.java
+++ src/main/java/SnakeGame.java
@@ -3,17 +3,15 @@ import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.event.EventHandler;
-import javafx.geometry.Insets;
-import javafx.scene.*;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyEvent;
-import javafx.scene.shape.Rectangle;
-import javafx.scene.control.Label;
+import javafx.scene.Group;
+import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.media.AudioClip;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontPosture;
 import javafx.scene.text.FontWeight;
@@ -21,26 +19,44 @@ import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 
 public class SnakeGame extends Application {
     Scene gameScene;
     Group gameRoot;
-    int level = 1;
+    static int level = 1;
     int fruitNum = 0;
     int score = 0;
     Tile[][] gameBoard;
+    static ImageView[][] fruitTrack;
     Group tileGroup;
+
+    static ArrayList<Tile> snake;
+    static Group snakeGroup;
+    int direction; // 0 not set, 1 right, 2 left
+    int currDir;
+
     Text lvlText = new Text(1070, 88, null);
-    Text fruitText = new Text(1070, 158, "Fruits:  " + fruitNum);
-    Text scoreText = new Text(1070, 228, "Score:  " + score);
+    Text fruitText = new Text(1070, 158, null);
+    Text scoreText = new Text(1070, 228, null);
     Text timeText = new Text(1070, 298, null);
     int timeCount;
+    int countDown = 25;
+    boolean eaten = false;
+
 
     Timeline timeline = new Timeline();
 
     public void setGameScene() {
         gameBoard = new Tile[30][30];
+        fruitTrack = new ImageView[30][30];
         tileGroup = new Group();
+        snake = new ArrayList<Tile>();
+        snakeGroup = new Group();
+        direction = 0;
+        currDir = 0;
         // Add background 800 x 800
         Rectangle background = new Rectangle(240,0,800,800);
         background.setFill(Color.valueOf("#45a5b5"));
@@ -59,17 +75,21 @@ public class SnakeGame extends Application {
                 tileGroup.getChildren().add(tile);
             }
         }
-        gameRoot.getChildren().add(tileGroup);
-
 
         // Add fruits
         for(int i = 0; i < level; ++i) {
-            gameRoot.getChildren().add(gameBoard[0+i][0+i].addFruit());
-            gameRoot.getChildren().add(gameBoard[29-i][0+i].addFruit());
-            gameRoot.getChildren().add(gameBoard[0+i][29-i].addFruit());
-            gameRoot.getChildren().add(gameBoard[29-i][29-i].addFruit());
-            gameRoot.getChildren().add(gameBoard[15+i][29-i].addFruit());
+            tileGroup.getChildren().add(gameBoard[0+i][0+i].addFruit());
+            tileGroup.getChildren().add(gameBoard[29-i][0+i].addFruit());
+            tileGroup.getChildren().add(gameBoard[0+i][29-i].addFruit());
+            tileGroup.getChildren().add(gameBoard[29-i][29-i].addFruit());
+            tileGroup.getChildren().add(gameBoard[15-i][29-i].addFruit());
         }
+        gameRoot.getChildren().add(tileGroup);
+        // Add snake
+        gameBoard[14][14].initSnake();
+        gameBoard[15][14].initSnake();
+        gameRoot.getChildren().add(snakeGroup);
+
         // Add Text
         Group textGroup = new Group();
         // level
@@ -77,12 +97,13 @@ public class SnakeGame extends Application {
         lvlText.setFont(Font.font("Ariel",FontWeight.EXTRA_BOLD, FontPosture.ITALIC,40));
         lvlText.setFill(Color.WHITE);
         // fruits
+        fruitText.setText("Fruits:  " + fruitNum);
         fruitText.setFont(Font.font("Ariel",FontWeight.EXTRA_BOLD, FontPosture.ITALIC,35));
         fruitText.setFill(Color.WHITE);
         // score
+        scoreText.setText("Score:  " + score);
         scoreText.setFont(Font.font("Ariel",FontWeight.EXTRA_BOLD, FontPosture.ITALIC,35));
         scoreText.setFill(Color.WHITE);
-
         // time
         if (level == 3) {
             timeText.setText("Time:  0");
@@ -101,6 +122,11 @@ public class SnakeGame extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        String eatSound = getClass().getClassLoader().getResource("eating.mp3").toString();
+        AudioClip eatClip = new AudioClip(eatSound);
+
+        String crushSound = getClass().getClassLoader().getResource("crush.mp3").toString();
+        AudioClip crushClip = new AudioClip(crushSound);
         // intro scene
         Image introImage = new Image("intro.png", 1300, 805, true, true);
         ImageView introImageView = new ImageView(introImage);
@@ -122,18 +148,134 @@ public class SnakeGame extends Application {
 
         // clock
         timeline.getKeyFrames().addAll(
-                new KeyFrame(Duration.seconds(1), event -> {
-                    if(level == 3) {
-                        ++timeCount;
-                        timeText.setText("Time:  " + timeCount);
-                    } else {
-                        if(timeCount == 1) {
-                            ++level;
-                            setGameScene();
-                        } else {
-                            --timeCount;
-                            timeText.setText("Time:  " + timeCount);
+                new KeyFrame(Duration.millis(40), event -> {
+                    Tile head = snake.get(snake.size()-1);
+                    Tile tail = snake.get(0);
+                    try {
+                        // if the body is full, change currDir based on direction
+                        if(head.snakeBody.getWidth() == head.tileSize && head.snakeBody.getHeight() == head.tileSize) {
+                            // Fruit update
+                            if(head.isFruit == true) head.isFruit = false;
+                            eaten = false;
+                            // Remove the tail if it is already empty
+                            if(tail.snakeBody.getWidth() == 0 || tail.snakeBody.getHeight() == 0) {
+                                snake.get(0).snakeBody.setHeight(0);
+                                snake.get(0).snakeBody.setWidth(0);
+                                snake.get(0).snakeBody.setX(snake.get(0).xPos);
+                                snake.get(0).snakeBody.setY(snake.get(0).yPos);
+                                snake.get(0).isSnake = false;
+                                snake.remove(0);
+                            }
+                            // Determine what is the next tile to go, which throws exception if the wall is hit
+                            Tile nextHead = head;
+                            if(direction == 0) {
+                                // not set
+                                if(currDir == 0) {
+                                    nextHead = gameBoard[1+head.x][head.y];
+                                } else if(currDir == 1) {
+                                    nextHead = gameBoard[head.x][1+head.y];
+                                } else if(currDir == 2) {
+                                    nextHead = gameBoard[head.x-1][head.y];
+                                } else if(currDir == 3) {
+                                    nextHead = gameBoard[head.x][head.y-1];
+                                }
+                                // currDir does not change
+                            } else if(direction == 1) {
+                                // turn right
+                                if(currDir == 0) {
+                                    nextHead = gameBoard[head.x][head.y+1];
+                                } else if(currDir == 1) {
+                                    nextHead = gameBoard[head.x-1][head.y];
+                                } else if(currDir == 2) {
+                                    nextHead = gameBoard[head.x][head.y-1];
+                                } else if(currDir == 3) {
+                                    nextHead = gameBoard[head.x+1][head.y];
+                                }
+                                if(currDir == 3) currDir = 0;
+                                else currDir += 1;
+
+                            } else if(direction == 2) {
+                                // turn left
+                                if(currDir == 0) {
+                                    nextHead = gameBoard[head.x][head.y-1];
+                                } else if(currDir == 1) {
+                                    nextHead = gameBoard[head.x+1][head.y];
+                                } else if(currDir == 2) {
+                                    nextHead = gameBoard[head.x][head.y+1];
+                                } else if(currDir == 3) {
+                                    nextHead = gameBoard[head.x-1][head.y];
+                                }
+                                if(currDir == 0) currDir = 3;
+                                else currDir -= 1;
+                            }
+                            direction = 0;
+                            // Check if it hit itself
+                            if(snake.contains(nextHead)) {
+                                Exception e = new Exception();
+                                throw e;
+                            }
+                            // if nothing is in front, we just go ahead
+                            snake.add(nextHead);
                         }
+                        //  Update head and tail)
+                        head = snake.get(snake.size()-1);
+                        tail = snake.get(0);
+                        // increase the head
+                        head.addSnake(currDir);
+                        // Check if there is a fruit
+                        if(head.isFruit) {
+                            if(eaten == false) {
+                                // remove the fruit
+                                tileGroup.getChildren().remove(fruitTrack[head.x][head.y]);
+                                // Add fruit number and score
+                                eatClip.play();
+                                fruitNum++;
+                                fruitText.setText("Fruit:  " + fruitNum);
+                                score+=level*10;
+                                scoreText.setText("Score:  " + score);
+
+                                // randomly spawn a fruit
+                                Random rand = new Random();
+                                int newX = rand.nextInt(30);
+                                int newY = rand.nextInt(30);
+                                while(snake.contains(gameBoard[newX][newY]) || gameBoard[newX][newY].isFruit == true) {
+                                    newX = rand.nextInt(30);
+                                    newY = rand.nextInt(30);
+                                }
+                                tileGroup.getChildren().add(gameBoard[newX][newY].addFruit());
+                            }
+                            eaten = true;
+                        } else {
+                            // cut the tail
+                            if(snake.get(1).x == tail.x) {
+                                tail.reduceSnake(2+tail.y-snake.get(1).y);
+                            } else if(snake.get(1).y == tail.y) {
+                                tail.reduceSnake(1+tail.x-snake.get(1).x);
+                            }
+                        }
+
+                    } catch (Exception e) {
+                        timeline.pause();
+                        crushClip.play();
+                        primaryStage.setScene(terScene);
+                    }
+                }),
+                new KeyFrame(Duration.millis(40), event -> {
+                    --countDown;
+                    if(countDown == 0) {
+                        if(level == 3) {
+                            ++timeCount;
+                            timeText.setText("Time:  " + timeCount);
+                        } else {
+                            if(timeCount == 1) {
+                                ++level;
+                                setGameScene();
+                            } else {
+                                --timeCount;
+                                timeText.setText("Time:  " + timeCount);
+                            }
+                        }
+                        countDown = 25;
                     }
                 }));
         timeline.setCycleCount(Animation.INDEFINITE);
@@ -177,6 +319,10 @@ public class SnakeGame extends Application {
                 } else if(event.getCode() == KeyCode.Q) {
                     timeline.pause();
                     primaryStage.setScene(terScene);
+                } else if(event.getCode() == KeyCode.RIGHT) {
+                    if(direction == 0) direction = 1;
+                } else if(event.getCode() == KeyCode.LEFT) {
+                    if(direction == 0) direction = 2;
                 }
             }
         });
--- src/main/java/Tile.java
+++ src/main/java/Tile.java
@@ -1,19 +1,30 @@
+import javafx.scene.Group;
 import javafx.scene.image.ImageView;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
 public class Tile extends Rectangle {
+    int x;
+    int y;
+    int tileSize;
     int xPos;
     int yPos;
     boolean isFruit;
+    boolean isSnake;
+    boolean col;
+    Rectangle snakeBody;
 
     Tile(int xCoor, int yCoor, int size, boolean colour, int fromX, int fromY) {
+        x = xCoor;
+        y = yCoor;
         xPos = xCoor * size + fromX;
         yPos = yCoor * size + fromY;
+        tileSize = size;
         isFruit = false;
+        isSnake = false;
+        col = colour;
         setWidth(size);
         setHeight(size);
-
         relocate(xCoor * size + fromX, yCoor * size + fromY);
 
         //#afeeee
@@ -21,17 +32,75 @@ public class Tile extends Rectangle {
         //#b1e9e4
         setFill(colour ? Color.valueOf("#e0ffff") : Color.valueOf("#c3eaea"));
 
+        snakeBody = new Rectangle(xPos,yPos,0,0);
+        snakeBody.setFill(Color.valueOf("#f69100"));
+        SnakeGame.snakeGroup.getChildren().add(snakeBody);
     }
 
-    ImageView addFruit() {
+    public ImageView addFruit() {
         isFruit = true;
         ImageView imageView = new ImageView("apple.png");
         imageView.setFitWidth(26);
         imageView.setFitHeight(26);
         imageView.setX(xPos);
         imageView.setY(yPos);
+        // track the fruit
+        SnakeGame.fruitTrack[x][y] = imageView;
 
         return imageView;
     }
+
+    public void initSnake() {
+        isSnake = true;
+        snakeBody.setWidth(tileSize);
+        snakeBody.setHeight(tileSize);
+        SnakeGame.snake.add(this);
+    }
+
+    public void addSnake(int dir) {
+        double speed = (double)tileSize/Math.pow(2,(4-SnakeGame.level));
+        if(dir == 0) {
+            // right
+            snakeBody.setWidth(snakeBody.getWidth()+speed);
+            snakeBody.setHeight(tileSize);
+        } else if (dir == 1) {
+            // down
+            snakeBody.setWidth(tileSize);
+            snakeBody.setHeight(snakeBody.getHeight()+speed);
+        } else if (dir == 2) {
+            // left
+            snakeBody.setWidth(snakeBody.getWidth()+speed);
+            snakeBody.setHeight(tileSize);
+            snakeBody.setX(xPos+tileSize-snakeBody.getWidth());
+        } else if (dir == 3) {
+            // up
+            snakeBody.setWidth(tileSize);
+            snakeBody.setHeight(snakeBody.getHeight()+speed);
+            snakeBody.setY(yPos+tileSize-snakeBody.getHeight());
+        }
+    }
+
+    public void reduceSnake(int dir) {
+        double speed = (double)tileSize/Math.pow(2,(4-SnakeGame.level));
+        if(dir == 0) {
+            // right
+            snakeBody.setWidth(snakeBody.getWidth()-speed);
+            snakeBody.setHeight(tileSize);
+            snakeBody.setX(xPos+tileSize-snakeBody.getWidth());
+        } else if (dir == 1) {
+            // down
+            snakeBody.setWidth(tileSize);
+            snakeBody.setHeight(snakeBody.getHeight()-speed);
+            snakeBody.setY(yPos+tileSize-snakeBody.getHeight());
+        } else if (dir == 2) {
+            // left
+            snakeBody.setWidth(snakeBody.getWidth()-speed);
+            snakeBody.setHeight(tileSize);
+        } else if (dir == 3) {
+            snakeBody.setWidth(tileSize);
+            snakeBody.setHeight(snakeBody.getHeight()-speed);
+        }
+    }
+
 
 }
